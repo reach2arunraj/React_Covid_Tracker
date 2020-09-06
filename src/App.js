1ed6c3aa07b5651210bd7815c78790b577da6401
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-09-06 12:17:30 +0530

    sorting the table data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import {FormControl, Select, MenuItem, Card, CardContent} from '@material-ui/core';
 import InfoBox from "./InfoBox"
 import Map from "./Map"
+import Table from "./Table"
+import { sortData } from './util';
 
 
 function App() {
   const [ countries, setCountries ] = useState([])
   const [ country , setCountry ] = useState("worldwide")
   const [ countryInfo, setCountryInfo ] = useState({})
+  const [ tableData, setTableData ] = useState([])
 
   useEffect(() =>{
     fetch("https://disease.sh/v3/covid-19/all")
@@ -28,6 +31,9 @@ function App() {
             name:country.country,
             value:country.countryInfo.iso2
           }));
+
+          const sortedData = sortData(data)
+          setTableData(sortedData);
           setCountries(countries)
       })
     }
@@ -82,6 +88,7 @@ console.log(countryInfo)
       <Card className="app__right">
         <CardContent>
           <h3>Live cases by country</h3>
+          <Table countries={tableData} />
           <h3>World wide new cases</h3>
         </CardContent>
       </Card>
